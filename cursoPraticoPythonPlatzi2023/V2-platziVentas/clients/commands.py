f53ee0361578a254e7db468f8feb78a0daeb992a
Carlos Yonusg <cyonusgdev@gmail.com>
2023-05-18 00:26:00 -0400

    .

---
+++ cursoPraticoPythonPlatzi2023/V2-platziVentas/clients/commands.py
@@ -0,0 +1,34 @@
+import click
+
+@click.group()
+def clients():
+    """ Manages the clients lifecycle"""
+    pass
+
+@clients.command()
+@click.pass_context
+def create(ctx, name, company, email, position):
+    """Creates a new client"""
+
+
+@clients.command()
+@click.pass_context
+def list(ctx):
+    """List all clients"""
+    pass
+
+
+@clients.command()
+@click.pass_context
+def update(ctx, client_uid):
+    """Updated a client"""
+    pass
+
+
+@clients.command()
+@click.pass_context
+def delete(ctx, client_uid):
+    """Delete a client"""
+    pass
+
+all = clients
