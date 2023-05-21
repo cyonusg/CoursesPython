8747213df08267d05081f49629651f7fca4c4454
Carlos Yonusg <cyonusgdev@gmail.com>
2023-05-21 17:36:26 -0400

    virtualEnv

---
+++ cursoPraticoPythonPlatzi2023/V2-platziVentas/clients/services.py
@@ -0,0 +1,11 @@
+import csv
+from clients.models import Client
+class ClientService:
+
+    def __init__(self, table_name):
+        self.table_name = table_name
+
+    def create_client(self):
+        with open(self.table_name, mode='a') as f:
+            writer = csv.DictWriter(f, fieldnames=Client.schema())
+            writer.writerow(Client.to_dict())
--- cursoPraticoPythonPlatzi2023/V2-platziVentas/setup.py
+++ cursoPraticoPythonPlatzi2023/V2-platziVentas/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
+
 setup(
     name='pv',
     version='0.1',
